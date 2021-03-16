7329119a898d98d0e2101a5961aac23428ac1cdf
João <joaovcl10@hotmail.com>
2021-03-15 21:46:08 -0300

    Insering an example from class

---
+++ Javascript/exercicio.js
@@ -0,0 +1,50 @@
+function Pessoa(){
+
+    // Atributes
+    this.nome = nome;
+    this.cpf = cpf;
+    this.tipo = tipo;
+    this.altura = altura;
+    this.nascimento = this.nascimento;
+    this.peso = peso;
+    this.sexo = sexo;
+
+    //Methods
+    nomeCompleto(){
+
+    }
+    novoLoginSIGAA(){
+
+    }
+
+    novoEmailINstitucional(){
+
+
+    }
+
+    idadeAtual(){
+
+    }
+
+    dataNascimento(){
+
+    }
+
+    diaNascimento(){
+
+    }
+
+    mesNascimento(){
+
+    }
+
+    anoNascimento(){
+
+    }
+
+    imc(){
+
+
+    }
+
+}
